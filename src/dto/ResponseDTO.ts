49245935d023cd5e20e75cb3460c0c5774b1f120
NazariiB <nazikbodna3@gmail.com>
2023-03-21 15:45:32 +0200

    add Promise.all

--- src/dto/ResponseDTO.ts
+++ src/dto/ResponseDTO.ts
@@ -7,27 +7,3 @@ export interface ResponseInterface {
     name: string
     balance: BigNumber
 }
-
-// [
-//     {
-//         "tokenAddress": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
-//         "price": 1.0010093677644896,
-//         "symbol": "usdt",
-//         "name": "Tether2",
-//         "balance": "6297057973"
-//     },
-//     {
-//         "tokenAddress": "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
-//         "price": 0.9986337806971939,
-//         "symbol": "usdc",
-//         "name": "USD Coin",
-//         "balance": "308802989"
-//     },
-//     {
-//         "tokenAddress": "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
-//         "price": 1.0010093677644896,
-//         "symbol": "usdt",
-//         "name": "Tether",
-//         "balance": "6297057973"
-//     }
-// ]
--- src/getBalances/getBalancesByAddress.ts
+++ src/getBalances/getBalancesByAddress.ts
@@ -12,7 +12,7 @@ import { BigNumber } from 'bignumber.js';
 export async function getBalancesByAddress(address: string, network: string) {
     try {
         const tokensCursor = TokenModel.collection.find().sort({ priority: 1 });
-        const tokens: any = await tokensCursor.toArray();
+        const tokens: TokenInterface[] = await tokensCursor.toArray() as any;
 
         const web3 = new Web3(secret.suportedNetworks[network as keyof typeof secret.suportedNetworks][0]);
         const response: ResponseInterface[] = [];
@@ -20,28 +20,28 @@ export async function getBalancesByAddress(address: string, network: string) {
         const moralisChainName = secret.suportedNetworks[network as keyof typeof secret.suportedNetworks][1];
         const chain = EvmChain[moralisChainName as keyof typeof EvmChain] as EvmChain;
 
-        for (const token of tokens as TokenInterface[]) {
-            const tokenAddress = token.addresses[network];
-
-            const tokenPriceResult = await Moralis.EvmApi.token.getTokenPrice({
-                address: token.addresses[network],
-                chain,
-            });
-            const tokenPrice = BigNumber(tokenPriceResult.toJSON().usdPrice);
-
-            const contract = new web3.eth.Contract(contractAbi as AbiItem[], token.addresses[network]);
-            const tokenBalance = BigNumber(await contract.methods.balanceOf(address).call());
-            const tokenDecimals = BigNumber(await contract.methods.decimals().call());
-
-            if (tokenBalance.comparedTo(0)) {
-                response.push({
-                    tokenAddress: tokenAddress as string,
-                    price: tokenPrice,
-                    symbol: token.symbol,
-                    name: token.token_name,
-                    balance: BigNumber(tokenBalance.dividedBy(BigNumber(10).pow(tokenDecimals)).multipliedBy(tokenPrice).toFixed(2))
-                })
-            }
+        for (let i = 0; i < tokens.length; i += 10) {
+            await Promise.all(tokens.slice(i, i + 10).map(async (token) => {
+                const tokenAddress = token.addresses[network];
+                const contract = new web3.eth.Contract(contractAbi as AbiItem[], token.addresses[network]);
+                const [tokenPriceResult, tokenBalance, tokenDecimals] = await Promise.all([
+                    Moralis.EvmApi.token.getTokenPrice({
+                        address: token.addresses[network],
+                        chain,
+                    }), BigNumber(await contract.methods.balanceOf(address).call()),
+                    BigNumber(await contract.methods.decimals().call())
+                ])
+                const tokenPrice = BigNumber(tokenPriceResult.toJSON().usdPrice);
+                if (tokenBalance.comparedTo(0)) {
+                    response.push({
+                        tokenAddress: tokenAddress as string,
+                        price: tokenPrice,
+                        symbol: token.symbol,
+                        name: token.token_name,
+                        balance: BigNumber(tokenBalance.dividedBy(BigNumber(10).pow(tokenDecimals)).multipliedBy(tokenPrice).toFixed(2))
+                    })
+                }
+            }))
         }
 
         return response;
